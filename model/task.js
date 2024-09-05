aff446fc559e70ab8abd347dc53e1134bd102f9b
LeslieZT <lfzarate30@gmail.com>
2024-09-05 17:57:46 -0500

    feat: update project structure

---
+++ model/task.js
@@ -0,0 +1,34 @@
+export class Task {
+  constructor(params) {
+    const {
+      ownerId,
+      title,
+      description,
+      status,
+      category,
+      startDate, // '05-10-2024'
+      endDate,
+      participants = [],
+    } = params;
+
+    this.id = crypto.randomUUID();
+    this.ownerId = ownerId;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+    this.category = category;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.participants = [ownerId, ...participants];
+  }
+}
+
+export const taskModel = {
+  task_id: String(),
+  owner: String(),
+  description: String(),
+  title: String(),
+  category: String(),
+  status: String(),
+  start_date: Date(),
+};
