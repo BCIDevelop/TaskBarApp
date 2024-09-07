96b25e5e88af77c8ad47fac4cc8be88480485014
Hansel Chavez <chavezhansel2901@gmail.com>
2024-09-07 09:03:11 -0500

    fix: Replaced onClick in HTML with addEventListener in JS to fix bugs and improve code separation

--- js/index.js
+++ js/index.js
@@ -1,48 +1,49 @@
-function showNavBar() {
-    document.getElementById("navbar").classList.add("menu-show");
-    document.getElementById("nav-overlay").classList.add("overlay-show");
-}
-function closeNavBar() {
-    document.getElementById("nav-overlay").classList.remove("overlay-show");
-    document.getElementById("navbar").classList.remove("menu-show");
-}
-function showFilter() {
-    document.querySelector(".filters-container").classList.toggle("visible");
-    document.querySelector(".orderby-container").classList.remove("visible");
-}
-function showOrderBy() {
-    document.querySelector(".orderby-container").classList.toggle("visible");
-    document.querySelector(".filters-container").classList.remove("visible");
-}
-function closeCreateTask(event) {
-    if (
-        (event.target && event.target.id === "create-task-overlay") ||
-        event.target.id === "close-modal-task"
-    ) {
-        document
-            .getElementById("create-task-overlay")
-            .classList.remove("overlay-show");
-    }
-}
-function showCreateTask() {
-    document
-        .getElementById("create-task-overlay")
-        .classList.add("overlay-show");
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    const overlay = document.getElementById("create-task-overlay");
+    const taskOverlay = document.getElementById("create-task-overlay");
     const closeModalButton = document.getElementById("close-modal-task");
-
-    // Cierra el modal si se hace clic en el overlay (fuera del modal)
-    overlay.addEventListener("click", function (event) {
+    const btnCloseNav = document.getElementById("btn_close_nav");
+    const btnShowNav = document.getElementById("btn_show-nav");
+    const navOverlay = document.getElementById("nav-overlay");
+    const btnFilter = document.getElementById("button_filter");
+    const btnOrderBy = document.getElementById("button_orderby");
+    const btnCreateTask = document.getElementById("btn_create_task");
+    btnCreateTask.addEventListener("click", () => {
+        taskOverlay.classList.add("overlay-show");
+    });
+    btnOrderBy?.addEventListener("click", () => {
+        document
+            .querySelector(".orderby-container")
+            .classList.toggle("visible");
+        document
+            .querySelector(".filters-container")
+            .classList.remove("visible");
+    });
+    btnFilter?.addEventListener("click", () => {
+        document
+            .querySelector(".filters-container")
+            .classList.toggle("visible");
+        document
+            .querySelector(".orderby-container")
+            .classList.remove("visible");
+    });
+    navOverlay?.addEventListener("click", () => {
+        document.getElementById("nav-overlay").classList.remove("overlay-show");
+        document.getElementById("navbar").classList.remove("menu-show");
+    });
+    btnCloseNav?.addEventListener("click", () => {
+        document.getElementById("nav-overlay").classList.remove("overlay-show");
+        document.getElementById("navbar").classList.remove("menu-show");
+    });
+    btnShowNav?.addEventListener("click", () => {
+        document.getElementById("navbar").classList.add("menu-show");
+        document.getElementById("nav-overlay").classList.add("overlay-show");
+    });
+    taskOverlay?.addEventListener("click", function (event) {
         if (event.target.id === "create-task-overlay") {
-            overlay.classList.remove("overlay-show");
+            taskOverlay.classList.remove("overlay-show");
         }
     });
-
-    // Cierra el modal si se hace clic en el botón de cerrar
-    closeModalButton.addEventListener("click", function () {
-        overlay.classList.remove("overlay-show");
+    closeModalButton?.addEventListener("click", function () {
+        taskOverlay.classList.remove("overlay-show");
     });
 });
